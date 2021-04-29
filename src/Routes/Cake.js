2bb68cbe800c8d2f69236fe9a91d3884b7f87a08
seoyeonko <seoyeon99o@naver.com>
2021-04-29 22:18:01 +0900

    cake gif 추가

--- src/Routes/Cake.js
+++ src/Routes/Cake.js
@@ -13,11 +13,15 @@ const Player = styled(ReactPlayer)`
   margin: 0px auto;
   /* text-align: center; */
 `;
+const CakeGif = styled.img`
+  display: block;
+  margin: 0px auto;
+`;
 
 const Cake = () => {
   return (
     <Container>
-      Cake그림 집어넣고, 노래 삽입
+      <CakeGif src="/Images/cake.gif" alt="cake_play" width="50%" />
       <Player
         url="https://www.youtube.com/watch?v=42Gtm4-Ax2U"
         width="50vw"
